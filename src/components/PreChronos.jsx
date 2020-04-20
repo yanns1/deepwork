fd4819cc0697c35e8f0effff5c1c4ecde17543b4
Yann Salmon <coolcode.ys@gmail.com>
2020-04-20 14:45:30 +0200

    Adding cursor on account icon

--- src/components/PreChronos.jsx
+++ src/components/PreChronos.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from 'react';
-import PropTypes from 'prop-types';
 import { ThemeContext } from './context/ThemeContext.jsx';
 import { AuthContext } from './context/AuthContext.jsx';
 
